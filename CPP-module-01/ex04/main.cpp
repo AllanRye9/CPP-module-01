0b091ee2b26aaf8315b0fc7a67d743c123c566b9
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2024-10-03 17:21:05 +0000

    files updated

--- CPP-module-01/ex04/main.cpp
+++ CPP-module-01/ex04/main.cpp
@@ -1,44 +1,65 @@
 #include "copyData.hpp"
 
-static void getStreams(str &line, str first, str second)
+static str getStreams(const str &line, const str &s1, const str &s2)
 {
-    size_t i = 0;
+    str result;
+    size_t pos = 0;
+    size_t s1Length = s1.length();
 
-    while((i = line.find(first, i) ) != str::npos)
+    while (pos < line.length())
     {
-        line = line.substr(0, i) + second + line.substr(i + first.length());
-            i += second.length();
+        size_t found = line.find(s1, pos);
+        if (found != str::npos)
+        {
+            result += line.substr(pos, found - pos);
+            result += s2;
+            pos = found + s1Length;
+        }
+        else
+        {
+            result += line.substr(pos);
+            break;
+        }
     }
+    return result;
 }
 
 int main(int ac, char **av)
 {
-    if (!(ac == 4))
+    if (ac != 4)
     {
-        std::cout << "\033[31m" << "Wrong Format <program name> : <filename> <file 1> <file 2>" << "\033[0m" <<  std::endl;
-        return 0;
+        std::cout << "\033[31m" << "Wrong Format. Usage: <program name> <filename> <file 1> <file 2>" << "\033[0m" << std::endl;
+        return 1;
     }
+
     str fileName = av[1];
     str inputA = av[2];
     str inputB = av[3];
-    str data = fileName + ".replace";
     str line;
-    std::ifstream input(fileName.c_str());
-    std::ofstream outPut(data.c_str());
-    if (input.is_open())
-    {
-        while (std::getline(input, line))
-        {
-            getStreams(line, inputA, inputB);
-            outPut << line;
-            if (!input.eof())
-                outPut << std::endl;
+    str data = fileName + ".replace";
 
-        }
-        input.close();
-        outPut.close();
+    std::ifstream input(fileName.c_str());
+    if (!input.is_open())
+    {
+        std::cout << "\033[31m" << "File cannot be opened" << "\033[0m" << std::endl;
+        return 1;
     }
-    else
-        std::cout << "\033[31m" << "File cannot be openned" << "\033[0m" << std::endl;
+
+    std::ofstream outPut(data.c_str());
+    if (!outPut.is_open())
+    {
+        std::cout << "\033[31m" << "Output file cannot be opened" << "\033[0m" << std::endl;
+        return 1;
+    }
+
+    while (std::getline(input, line))
+    {
+        str modifiedLine = getStreams(line, inputA, inputB);
+        outPut << modifiedLine;
+        if (!input.eof())
+            outPut << std::endl;
+    }
+    input.close();
+    outPut.close();
     return 0;
 }
